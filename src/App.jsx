bf6d73807bcaa017c1b7aa3c4f4388a7694a879f
Vishal Yadav <vishalyxo@gmail.com>
2024-01-24 11:32:39 +0530

    Your commit message here

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='bg-red-600'> Start the project </div>
+    </>
+  )
+}
+
+export default App
